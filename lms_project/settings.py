8bb636ad874be9f2e22b78cdb418e2bddc74682f
Olabode Ezekiel <ayoeze191@gmail.com>
2026-06-26 22:51:54 +0100

    render.yml file for celery

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -182,7 +182,7 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
 }
 
-CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
@@ -194,7 +194,7 @@ CELERY_TASK_TIME_LIMIT = 30 * 60  # 30 minutes
 CELERY_TASK_SOFT_TIME_LIMIT = 20 * 60  # 20 minutes
 
 # Store task results in database
-CELERY_RESULT_BACKEND = 'django-db'
+CELERY_RESULT_BACKEND = os.gentenv('CELERY_RESULT_BACKEND', 'django-db')
 
 # Task annotations (optional)
 CELERY_TASK_ANNOTATIONS = {
